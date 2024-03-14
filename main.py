b19cb29c317b33faf51d5a216d9ea7e15f863045
FearghalM <femcmorrow@gmail.com>
2024-03-14 19:09:32 +0000

    Update .gitignore and main.py

--- main.py
+++ main.py
@@ -3,30 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 import webbrowser
 
+
 def onclickLink(url):
     # Open the link in the default web browser
     webbrowser.open_new(url)
+
 
 def scrapeWebPage():
     # Code to scrape web page
     url = urlEntry.get()
     response = requests.get(url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         title = soup.title.string
         # all links that start with https://
-        links = soup.find_all('a', href=True)
+        links = soup.find_all("a", href=True)
         resultLabel.config(text=f"Title: {title}")
-        #show all links that start with https:// and make them clickable
+        # show all links that start with https:// and make them clickable
         linksResultLabel.config(text="Links:")
         for link in links:
-            if link.get('href').startswith('https'):
-                link_text = link.get('href')
+            if link.get("href").startswith("https"):
+                link_text = link.get("href")
                 link_label = tk.Label(root, text=link_text, fg="blue", cursor="hand2")
                 link_label.grid(sticky="w")
                 link_label.bind("<Button-1>", lambda url=link_text: onclickLink(url))
     else:
         resultLabel.config(text="Error accessing web page")
+
 
 # Code to create GUI
 root = tk.Tk()
